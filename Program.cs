3adc077af7b45da41aee6cf91172287b6537dd8d
Daniel Meisenheimer <dcmeisenheimer@crimson.ua.edu>
2021-08-31 18:14:49 -0500

    Menu System

--- Program.cs
+++ Program.cs
@@ -6,7 +6,64 @@ namespace BigAlFirstDay
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            GetMenuOptions();
+            System.Console.WriteLine("Thanks for playing Goodbye!");
+        }
+        static void GetMenuOptions()
+        {
+            int userChoice;
+            userChoice = DisplayMenu();
+            CheckChoice(userChoice);
+
+        }
+        static int DisplayMenu()
+        {
+            try
+            {
+                Console.Clear();
+                System.Console.WriteLine($"1. Show all Post \n2. Add a post \n3. Delete a post by ID \n4. Exit");
+                return int.Parse(Console.ReadLine());
+            }
+            catch (System.Exception e)
+            {
+                System.Console.WriteLine(e.Message);
+                return 0;
+            }
+            
+        }
+        static void CheckChoice(int userChoice)
+        {
+            while (userChoice != 4)
+            {
+                if(userChoice == 1)
+                {
+                    System.Console.WriteLine($"Choice 1 selected \nPress any key to continue");
+                    Console.ReadLine();
+                    GetMenuOptions();
+                    break;
+                }
+                else if(userChoice == 2)
+                {
+                    System.Console.WriteLine($"Choice 2 selected \nPress any key to continue");
+                    Console.ReadLine();
+                    GetMenuOptions();
+                    break;
+                }
+                else if(userChoice == 3)
+                {
+                    System.Console.WriteLine($"Choice 3 selected \nPress any key to continue");
+                    Console.ReadLine();
+                    GetMenuOptions();
+                    break;
+                }
+                else
+                {
+                    System.Console.WriteLine($"Not a valid choice please try again \nPress any key to continue...");
+                    Console.ReadKey();   
+                    GetMenuOptions();
+                    break;
+                }
+            }
         }
     }
 }
